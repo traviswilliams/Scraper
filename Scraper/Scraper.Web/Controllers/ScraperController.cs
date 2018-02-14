b34531df28f8624f08c65634cf43b825e3025fae
twilliams <twilliams@xignite.com>
2018-02-13 16:13:44 -0800

    Add simple WebApi project with blank Scraper controller.

---
+++ Scraper/Scraper.Web/Controllers/ScraperController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http;
+
+namespace Scraper.Web.Controllers
+{
+    public class ScraperController : ApiController
+    {
+
+    }
+}
---
+++ Scraper/Scraper.Web/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Scraper.Web
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
